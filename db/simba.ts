7193098e42c313dad2a2511653ccf943426c1207
gvdes <desarrollo@grupovizcarra.mx>
2022-02-25 19:28:29 -0600

    Optimizacion de la sincronizacion de productos

--- db/simba.ts
+++ db/simba.ts
@@ -14,70 +14,50 @@ export const SIMBA = async()=>{
         console.time('t1');
         const simbainit = `[${moment().format("YYYY/MM/DD h:mm:ss")}]: Simba ha iniciado...`;
         console.log(`\n${simbainit}`);
-        let WRHGEN:any=[], WRHEXH:any=[], WRHFDT:any=[] ;
+        let rset:any = [];
 
-        let rset:any = { GEN:[], EXH:[], DES:[], FDT:[] };
+        const rows:Array<any> = await fsol.query(`
+            SELECT F_STO.ARTSTO AS CODIGO,
+            GEN.ACTSTO AS GENSTOCK,
+            EXH.ACTSTO AS EXHSTOCK,
+            FDT.ACTSTO AS FDTSTOCK,
+            (GEN.ACTSTO + EXH.ACTSTO + FDT.ACTSTO) AS STOCK 
+            FROM 
+            (
+                (
+                    (F_STO INNER JOIN F_STO AS GEN ON GEN.ARTSTO = F_STO.ARTSTO)
+                    INNER JOIN F_STO AS EXH  ON EXH.ARTSTO = F_STO.ARTSTO
+                )
+                INNER JOIN F_STO AS FDT  ON FDT.ARTSTO = F_STO.ARTSTO
+            )
+            WHERE
+            (GEN.ALMSTO="GEN" AND EXH.ALMSTO="EXH" AND FDT.ALMSTO="FDT")
+            GROUP BY F_STO.ARTSTO, GEN.ACTSTO, EXH.ACTSTO, FDT.ACTSTO;
+        `);
 
-        const rows:Array<any> = await fsol.query('SELECT ALMSTO,ARTSTO,ACTSTO FROM F_STO ORDER BY ARTSTO;');
-
-        console.log(rows.length);
-
-        WRHGEN = rows.filter( r => r.ALMSTO=="GEN");
-        WRHEXH = rows.filter( r => r.ALMSTO=="EXH");
-        WRHFDT = rows.filter( r => r.ALMSTO=="FDT");
-        // WRHDES = rows.filter( r => r.ALMSTO=="DES");
-
-        console.log("GEN",WRHGEN.length);
-        console.log("EXB",WRHEXH.length);
-        console.log("FDT",WRHFDT.length);
-        console.log("WORKPOINT",workpoint.id);
-
-        if(WRHGEN.length){
-            console.log("Syncronizando Almacen GENERAL...");
-            for await (const row of WRHGEN) {
+        
+        if(rows.length){
+            console.log("Syncronizando ALMACENES...");
+            console.log(rows.length, "filas");
+            for await (const row of rows) {
                 const [results]:any = await vizapi.query(`
                     UPDATE product_stock STO
                         INNER JOIN products P ON P.id = STO._product
                         INNER JOIN workpoints W ON W.id = STO._workpoint
-                    SET gen=${row.ACTSTO}
-                    WHERE P.code="${row.ARTSTO}" AND W.id="${workpoint.id}";
+                    SET
+                        STO.stock="${row.STOCK}",
+                        STO.exh="${row.EXHSTOCK}",
+                        STO.gen="${row.GENSTOCK}",
+                        STO.fdt="${row.FDTSTOCK}"
+                    WHERE
+                        P.code="${row.CODIGO}" AND W.id="${workpoint.id}";
                 `);
-                if(results.changedRows>0){ rset.GEN.push({code:row.ARTSTO}); }
+                if(results.changedRows>0){ rset.push({code:row.CODIGO}); }
             }
         }
 
-        if(WRHEXH.length){
-            console.log("Syncronizando Almacen EXHIBICION...");
-            for await (const row of WRHEXH) {
-                const [results]:any = await vizapi.query(`
-                    UPDATE product_stock STO
-                        INNER JOIN products P ON P.id = STO._product
-                        INNER JOIN workpoints W ON W.id = STO._workpoint
-                    SET exh=${row.ACTSTO}
-                    WHERE P.code="${row.ARTSTO}" AND W.id="${workpoint.id}";
-                `);
-                if(results.changedRows>0){ rset.EXH.push({code:row.ARTSTO}); }
-            }
-        }
-
-        if(WRHFDT.length){
-            console.log("Syncronizando Almacen FIN DE TEMPORADA...");
-            for await (const row of WRHFDT) {
-                const [results]:any = await vizapi.query(`
-                    UPDATE product_stock STO
-                        INNER JOIN products P ON P.id = STO._product
-                        INNER JOIN workpoints W ON W.id = STO._workpoint
-                    SET fdt=${row.ACTSTO}
-                    WHERE P.code="${row.ARTSTO}" AND W.id="${workpoint.id}";
-                `);
-                if(results.changedRows>0){ rset.FDT.push({code:row.ARTSTO}); }
-            }
-        }
-
-        console.log("FILAS TOTALES:",(WRHGEN.length+WRHEXH.length+WRHFDT.length));
-        console.log("ALMACEN GENERAL:",WRHGEN.length,"UPDATEDS:",rset.GEN.length);
-        console.log("ALMACEN EXHIBICION:",WRHEXH.length,"UPDATEDS:",rset.EXH.length);
-        console.log("ALMACEN FINDETEMPO:",WRHFDT.length,"UPDATEDS:",rset.FDT.length);
+        console.log("FILAS TOTALES:",rows.length);
+        console.log("UPDATEDS:",rset.length);
 
         const simbaends = `[${moment().format("YYYY/MM/DD h:mm:ss")}]: Simba ha finalizado.`;
         console.timeEnd('t1');
--- dist/db/simba.js
+++ dist/db/simba.js
@@ -25,7 +25,7 @@ const moment_1 = __importDefault(require("moment"));
 const vizapi_1 = __importDefault(require("../db/vizapi"));
 const fsol = node_adodb_1.default.open(`Provider=Microsoft.ACE.OLEDB.12.0;Data Source=${process.env.FSOLDB};Persist Security Info=False;`);
 const SIMBA = () => __awaiter(void 0, void 0, void 0, function* () {
-    var e_1, _a, e_2, _b, e_3, _c;
+    var e_1, _a;
     const hourstart = (0, moment_1.default)('08:55:00', 'hh:mm:ss');
     const hourend = (0, moment_1.default)('21:00:00', 'hh:mm:ss');
     const now = (0, moment_1.default)();
@@ -35,97 +35,58 @@ const SIMBA = () => __awaiter(void 0, void 0, void 0, function* () {
         console.time('t1');
         const simbainit = `[${(0, moment_1.default)().format("YYYY/MM/DD h:mm:ss")}]: Simba ha iniciado...`;
         console.log(`\n${simbainit}`);
-        let WRHGEN = [], WRHEXH = [], WRHFDT = [];
-        let rset = { GEN: [], EXH: [], DES: [], FDT: [] };
-        const rows = yield fsol.query('SELECT ALMSTO,ARTSTO,ACTSTO FROM F_STO ORDER BY ARTSTO;');
-        console.log(rows.length);
-        WRHGEN = rows.filter(r => r.ALMSTO == "GEN");
-        WRHEXH = rows.filter(r => r.ALMSTO == "EXH");
-        WRHFDT = rows.filter(r => r.ALMSTO == "FDT");
-        // WRHDES = rows.filter( r => r.ALMSTO=="DES");
-        console.log("GEN", WRHGEN.length);
-        console.log("EXB", WRHEXH.length);
-        console.log("FDT", WRHFDT.length);
-        console.log("WORKPOINT", workpoint.id);
-        if (WRHGEN.length) {
-            console.log("Syncronizando Almacen GENERAL...");
+        let rset = [];
+        const rows = yield fsol.query(`
+            SELECT F_STO.ARTSTO AS CODIGO,
+            GEN.ACTSTO AS GENSTOCK,
+            EXH.ACTSTO AS EXHSTOCK,
+            FDT.ACTSTO AS FDTSTOCK,
+            (GEN.ACTSTO + EXH.ACTSTO + FDT.ACTSTO) AS STOCK 
+            FROM 
+            (
+                (
+                    (F_STO INNER JOIN F_STO AS GEN ON GEN.ARTSTO = F_STO.ARTSTO)
+                    INNER JOIN F_STO AS EXH  ON EXH.ARTSTO = F_STO.ARTSTO
+                )
+                INNER JOIN F_STO AS FDT  ON FDT.ARTSTO = F_STO.ARTSTO
+            )
+            WHERE
+            (GEN.ALMSTO="GEN" AND EXH.ALMSTO="EXH" AND FDT.ALMSTO="FDT")
+            GROUP BY F_STO.ARTSTO, GEN.ACTSTO, EXH.ACTSTO, FDT.ACTSTO;
+        `);
+        if (rows.length) {
+            console.log("Syncronizando ALMACENES...");
+            console.log(rows.length, "filas");
             try {
-                for (var WRHGEN_1 = __asyncValues(WRHGEN), WRHGEN_1_1; WRHGEN_1_1 = yield WRHGEN_1.next(), !WRHGEN_1_1.done;) {
-                    const row = WRHGEN_1_1.value;
+                for (var rows_1 = __asyncValues(rows), rows_1_1; rows_1_1 = yield rows_1.next(), !rows_1_1.done;) {
+                    const row = rows_1_1.value;
                     const [results] = yield vizapi_1.default.query(`
                     UPDATE product_stock STO
                         INNER JOIN products P ON P.id = STO._product
                         INNER JOIN workpoints W ON W.id = STO._workpoint
-                    SET gen=${row.ACTSTO}
-                    WHERE P.code="${row.ARTSTO}" AND W.id="${workpoint.id}";
+                    SET
+                        STO.stock="${row.STOCK}",
+                        STO.exh="${row.EXHSTOCK}",
+                        STO.gen="${row.GENSTOCK}",
+                        STO.fdt="${row.FDTSTOCK}"
+                    WHERE
+                        P.code="${row.CODIGO}" AND W.id="${workpoint.id}";
                 `);
                     if (results.changedRows > 0) {
-                        rset.GEN.push({ code: row.ARTSTO });
+                        rset.push({ code: row.CODIGO });
                     }
                 }
             }
             catch (e_1_1) { e_1 = { error: e_1_1 }; }
             finally {
                 try {
-                    if (WRHGEN_1_1 && !WRHGEN_1_1.done && (_a = WRHGEN_1.return)) yield _a.call(WRHGEN_1);
+                    if (rows_1_1 && !rows_1_1.done && (_a = rows_1.return)) yield _a.call(rows_1);
                 }
                 finally { if (e_1) throw e_1.error; }
             }
         }
-        if (WRHEXH.length) {
-            console.log("Syncronizando Almacen EXHIBICION...");
-            try {
-                for (var WRHEXH_1 = __asyncValues(WRHEXH), WRHEXH_1_1; WRHEXH_1_1 = yield WRHEXH_1.next(), !WRHEXH_1_1.done;) {
-                    const row = WRHEXH_1_1.value;
-                    const [results] = yield vizapi_1.default.query(`
-                    UPDATE product_stock STO
-                        INNER JOIN products P ON P.id = STO._product
-                        INNER JOIN workpoints W ON W.id = STO._workpoint
-                    SET exh=${row.ACTSTO}
-                    WHERE P.code="${row.ARTSTO}" AND W.id="${workpoint.id}";
-                `);
-                    if (results.changedRows > 0) {
-                        rset.EXH.push({ code: row.ARTSTO });
-                    }
-                }
-            }
-            catch (e_2_1) { e_2 = { error: e_2_1 }; }
-            finally {
-                try {
-                    if (WRHEXH_1_1 && !WRHEXH_1_1.done && (_b = WRHEXH_1.return)) yield _b.call(WRHEXH_1);
-                }
-                finally { if (e_2) throw e_2.error; }
-            }
-        }
-        if (WRHFDT.length) {
-            console.log("Syncronizando Almacen FIN DE TEMPORADA...");
-            try {
-                for (var WRHFDT_1 = __asyncValues(WRHFDT), WRHFDT_1_1; WRHFDT_1_1 = yield WRHFDT_1.next(), !WRHFDT_1_1.done;) {
-                    const row = WRHFDT_1_1.value;
-                    const [results] = yield vizapi_1.default.query(`
-                    UPDATE product_stock STO
-                        INNER JOIN products P ON P.id = STO._product
-                        INNER JOIN workpoints W ON W.id = STO._workpoint
-                    SET fdt=${row.ACTSTO}
-                    WHERE P.code="${row.ARTSTO}" AND W.id="${workpoint.id}";
-                `);
-                    if (results.changedRows > 0) {
-                        rset.FDT.push({ code: row.ARTSTO });
-                    }
-                }
-            }
-            catch (e_3_1) { e_3 = { error: e_3_1 }; }
-            finally {
-                try {
-                    if (WRHFDT_1_1 && !WRHFDT_1_1.done && (_c = WRHFDT_1.return)) yield _c.call(WRHFDT_1);
-                }
-                finally { if (e_3) throw e_3.error; }
-            }
-        }
-        console.log("FILAS TOTALES:", (WRHGEN.length + WRHEXH.length + WRHFDT.length));
-        console.log("ALMACEN GENERAL:", WRHGEN.length, "UPDATEDS:", rset.GEN.length);
-        console.log("ALMACEN EXHIBICION:", WRHEXH.length, "UPDATEDS:", rset.EXH.length);
-        console.log("ALMACEN FINDETEMPO:", WRHFDT.length, "UPDATEDS:", rset.FDT.length);
+        console.log("FILAS TOTALES:", rows.length);
+        console.log("UPDATEDS:", rset.length);
         const simbaends = `[${(0, moment_1.default)().format("YYYY/MM/DD h:mm:ss")}]: Simba ha finalizado.`;
         console.timeEnd('t1');
         console.log(`${simbaends}\n`);
